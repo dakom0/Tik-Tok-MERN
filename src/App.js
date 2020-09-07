776f0e4635fe0dd0db1b8a0c92eed884088210bb
DAMI KOMOLAFE <damik94@gmail.com>
2020-09-07 01:38:10 -0400

    front-end

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ function App() {
   return (
     <div className="app">
         <div className="app__videos">
-          <Video url={"https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4"}/>
-          <Video/>
-          <Video/>
-
-      </div>   
+          <Video channel={"@ssasangha"} description={"EPIC MERN Tik-Tok clone"} song={"Get 6 figs in 5 years"} 
+          likes={100} shares={20} messages={250}
+          url={"https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4"}/>
+          <Video channel={"@ssasangha"} description={"EPIC MERN Tik-Tok clone"} song={"Get 6 figs in 5 years"} 
+          likes={150} shares={20} messages={750}
+          url={"https://assets.mixkit.co/videos/preview/mixkit-young-sportsman-jumping-rope-at-home-5050-large.mp4"}/>          
+          <Video channel={"@ssasangha"} description={"EPIC MERN Tik-Tok clone"} song={"Get 6 figs in 5 years"} 
+          likes={100} shares={20} messages={250}
+          url={"https://assets.mixkit.co/videos/preview/mixkit-panting-dog-1548-large.mp4"}/>      </div>   
     </div>
   );
 }
